a866e712dd61c9cc9806ee23a53ca669387ce680
naoyama88 <naoyama88@gmail.com>
2019-09-14 12:10:19 -0700

    finding bug: still working

--- js/index.js
+++ js/index.js
@@ -12,29 +12,11 @@ const INIT_HERO_POSITION_X = 250;
 const INIT_HERO_POSITION_Y = 460;
 const LASER_FROM_X = 0;
 const LASER_FROM_Y = -120;
-const INIT_ENEMY_POSITION_Y = -40;
 
 const HERO_SIZE_X = 20;
 const HERO_SIZE_Y = 20;
 const LASER_SIZE_X = 2;
 const LASER_SIZE_Y = 50;
-const ENEMY_SIZE_X = 35;
-const ENEMY_SIZE_Y = 35;
-
-const SCREEN_LEFT = 20;
-const SCREEN_RIGHT = 480;
-const SCREEN_TOP = 20;
-const SCREEN_BOTTOM = 480;
-
-const FREQUENCY_OF_ENEMY_APPEARANCE_LV01 = 50;
-const FREQUENCY_OF_ENEMY_APPEARANCE_LV02 = 35;
-const FREQUENCY_OF_ENEMY_APPEARANCE_LV03 = 20;
-const FREQUENCY_OF_ENEMY_APPEARANCE_LV04 = 5;
-
-const ITERATIONS_LV01 = 0;
-const ITERATIONS_LV02 = 500;
-const ITERATIONS_LV03 = 1000;
-const ITERATIONS_LV04 = 1500;
 
 const BASIC_SCORE_POINT = 100;
 
@@ -52,45 +34,31 @@ class Game {
     }
 
     start() {
-        let hero = new Sprite(
-            "hero",
-            INIT_HERO_POSITION_X - HERO_SIZE_X / 2,
-            INIT_HERO_POSITION_Y,
-            HERO_SIZE_X,
-            HERO_SIZE_Y
-        );
-        let field = new Field(
-            SCREEN_LEFT,
-            SCREEN_TOP,
-            SCREEN_RIGHT,
-            SCREEN_BOTTOM,
-            hero
-        );
-        this.match = new Match(field);
+        this.match = new Match();
         loop();
     }
 
     handleControls() {
         if (this.controller.up && !this.match.ended) {
-            this.match.field.hero.y -= HERO_MOVEMENT;
+            this.match.hero.y -= HERO_MOVEMENT;
         }
         if (this.controller.down && !this.match.ended) {
-            this.match.field.hero.y += HERO_MOVEMENT;
+            this.match.hero.y += HERO_MOVEMENT;
         }
         if (this.controller.left && !this.match.ended) {
-            this.match.field.hero.x -= HERO_MOVEMENT;
+            this.match.hero.x -= HERO_MOVEMENT;
         }
         if (this.controller.right && !this.match.ended) {
-            this.match.field.hero.x += HERO_MOVEMENT;
+            this.match.hero.x += HERO_MOVEMENT;
         }
         if (this.controller.space && !this.match.ended) {
-            this.match.field.newLaser();
+            this.match.newLaser();
         }
         if (this.controller.enter && this.match.ended === true) {
             this.restart();
         }
 
-        this.match.field.ensureBounds(this.match.field.hero);
+        this.match.ensureBounds(this.match.hero);
     }
 
     gameOver() {
@@ -98,38 +66,23 @@ class Game {
             return;
         }
         this.match.ended = true;
-        // let heroElement = document.getElementById(this.match.field.hero.id);
         View.setHidden(heroElm);
         View.setVisible(elmGameOver);
         View.setVisible(elmRestart);
     }
 
     restart() {
-        for (let i = 0; i < this.match.field.enemies.length; i++) {
-            document.getElementById(this.match.field.enemies[i].id).remove();
-        }
-        for (let i = 0; i < this.match.field.lasers.length; i++) {
-            document.getElementById(this.match.field.lasers[i].id).remove();
+        if (this.match !== null) {
+            for (let i = 0; i < this.match.enemies.length; i++) {
+                document.getElementById(this.match.enemies[i].id).remove();
+            }
+            for (let i = 0; i < this.match.lasers.length; i++) {
+                document.getElementById(this.match.lasers[i].id).remove();
+            }
         }
 
-        let hero = new Sprite(
-            "hero",
-            INIT_HERO_POSITION_X - HERO_SIZE_X / 2,
-            INIT_HERO_POSITION_Y,
-            HERO_SIZE_X,
-            HERO_SIZE_Y
-        );
-        let field = new Field(
-            SCREEN_LEFT,
-            SCREEN_TOP,
-            SCREEN_RIGHT,
-            SCREEN_BOTTOM,
-            hero
-        );
-        this.match = new Match(field);
-        this.match.field.enemies = new Array();
-        this.match.field.lasers = new Array();
-        this.match.field.hero.setPosition();
+        this.match = new Match();
+        View.setPosition(this.match.hero.id, this.match.hero.x, this.match.hero.y);
         View.setVisible(heroElm);
         View.setHidden(elmGameOver);
         View.setHidden(elmRestart);
@@ -139,17 +92,174 @@ class Game {
 }
 
 class Match {
-    constructor(field) {
+    lastLoopRun;
+    iterations;
+    ended;
+    score;
+    left;
+    top;
+    right;
+    bottom;
+    enemies;
+    lasers;
+    hero;
+    createdLastLaserAt;
+    constructor() {
+        const SCREEN_LEFT = 20;
+        const SCREEN_RIGHT = 480;
+        const SCREEN_TOP = 20;
+        const SCREEN_BOTTOM = 480;
         this.lastLoopRun = 0;
         this.iterations = 0;
-        this.field = field;
         this.ended = false;
         this.score = 0;
+        this.left = SCREEN_LEFT;
+        this.top = SCREEN_TOP;
+        this.right = SCREEN_RIGHT;
+        this.bottom = SCREEN_BOTTOM;
+        this.enemies = new Array();
+        this.lasers = new Array();
+        this.hero = new Hero((this.right - this.left) / 2, this.bottom);
+        this.createdLastLaserAt = 0;
     }
 
     showScore() {
         if (!this.ended) {
             scoreElement.innerHTML = "SCORE: " + this.score;
+        }
+    }
+
+    pushEnemy(enemy) {
+        this.enemies[this.enemies.length] = enemy;
+    }
+
+    pushLaser(laser) {
+        this.lasers[this.lasers.length] = laser;
+    }
+
+    ensureBounds(sprite, ignoreY) {
+        if (sprite.x < this.left) {
+            sprite.x = this.left;
+        }
+        if (!ignoreY && sprite.y < this.top) {
+            sprite.y = this.top;
+        }
+        if (sprite.x + sprite.w > this.right) {
+            sprite.x = this.right - sprite.w;
+        }
+        if (!ignoreY && sprite.y + sprite.h > this.bottom) {
+            sprite.y = this.bottom - sprite.h;
+        }
+    }
+
+    addEnemy() {
+        const ITERATIONS_LV01 = 0;
+        const ITERATIONS_LV02 = 500;
+        const ITERATIONS_LV03 = 1000;
+        const ITERATIONS_LV04 = 1500;
+
+        const FREQUENCY_OF_ENEMY_APPEARANCE_LV01 = 50;
+        const FREQUENCY_OF_ENEMY_APPEARANCE_LV02 = 35;
+        const FREQUENCY_OF_ENEMY_APPEARANCE_LV03 = 20;
+        const FREQUENCY_OF_ENEMY_APPEARANCE_LV04 = 5;
+        // let interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV01;
+        let interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV04;
+        // if (iterations > ITERATIONS_LV04) {
+        //     interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV04;
+        // } else if (iterations > ITERATIONS_LV03) {
+        //     interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV03;
+        // } else if (iterations > ITERATIONS_LV02) {
+        //     interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV02;
+        // }
+
+        if (Util.getRandom(interval) === 0) {
+            let enemy = new Pawn("enemy" + Util.getRandom(10000000));
+            this.pushEnemy(enemy);
+            View.createElementInContainer(enemy.id, "enemy");
+        }
+    }
+
+    newLaser() {
+        if (this.createdLastLaserAt < 20) {
+            return;
+        }
+        this.createdLastLaserAt = 0;
+        const laserId = "laser" + Util.getRandom(10000000);
+        let laser = new NormalLaser(
+            laserId,
+            this.hero.x + this.hero.w / 2,
+            this.hero.y
+        );
+        this.pushLaser(laser);
+        View.createElementInContainer(laserId, "laser");
+    }
+
+    showSprites() {
+        View.setPosition(this.hero.id, this.hero.x, this.hero.y);
+        for (let i = 0; i < this.lasers.length; i++) {
+            View.setPosition(
+                this.lasers[i].id,
+                this.lasers[i].x,
+                this.lasers[i].y
+            );
+        }
+        for (let i = 0; i < this.enemies.length; i++) {
+            View.setPosition(
+                this.enemies[i].id,
+                this.enemies[i].x,
+                this.enemies[i].y
+            );
+        }
+    }
+
+    checkCollisions() {
+        for (let i = 0; i < this.enemies.length; i++) {
+            for (let y = 0; y < this.lasers.length; y++) {
+                if (intersects(this.lasers[y], this.enemies[i])) {
+                    document.getElementById(this.enemies[i].id).remove();
+                    this.enemies.splice(i, 1);
+                    i--;
+                    game.match.score += BASIC_SCORE_POINT;
+                    // remove laser
+                    document.getElementById(this.lasers[y].id).remove();
+                    this.lasers.splice(y, 1);
+                    y--;
+                }
+            }
+        }
+
+        for (let i = 0; i < this.enemies.length; i++) {
+            if (intersects(this.hero, this.enemies[i])) {
+                game.gameOver();
+            } else if (
+                this.enemies[i].y + this.enemies[i].h >=
+                this.bottom + 40
+            ) {
+                document.getElementById(this.enemies[i].id).remove();
+                this.enemies.splice(i, 1);
+                i--;
+            }
+        }
+
+        for (let y = 0; y < this.lasers.length; y++) {
+            if (this.lasers[y].y + this.lasers[y].h <= this.top) {
+                document.getElementById(this.lasers[y].id).remove();
+                this.lasers.splice(y, 1);
+                y--;
+            }
+        }
+    }
+
+    updatePosition() {
+        for (let i = 0; i < this.enemies.length; i++) {
+            this.enemies[i].y += 4;
+            this.enemies[i].x += Util.getRandom(7) - 3;
+            this.ensureBounds(this.enemies[i], true);
+        }
+
+        for (let i = 0; i < this.lasers.length; i++) {
+            this.lasers[i].y -= 12;
+            this.ensureBounds(this.lasers[i], true);
         }
     }
 }
@@ -213,159 +323,52 @@ class Sprite {
         this.w = w;
         this.h = h;
     }
+}
 
-    setPosition() {
-        let e = document.getElementById(this.id);
-        e.style.left = this.x + "px";
-        e.style.top = this.y + "px";
+class Hero extends Sprite {
+    constructor(x, bottom) {
+        const W = 20;
+        const H = 20;
+        super("hero", x - W / 2, bottom - H, W, H);
     }
 }
 
-class Field {
-    constructor(startX, startY, endX, endY, hero) {
-        this.startX = startX;
-        this.startY = startY;
-        this.endX = endX;
-        this.endY = endY;
-        this.enemies = new Array();
-        this.lasers = new Array();
-        this.hero = hero;
-        this.createdLastLaserAt = 0;
+class Enemy extends Sprite {
+    constructor(id, x, y, w, h) {
+        super(id, x, y, w, h);
     }
+}
 
-    pushEnemy(enemy) {
-        this.enemies[this.enemies.length] = enemy;
-    }
-
-    pushLaser(laser) {
-        this.lasers[this.lasers.length] = laser;
-    }
-
-    ensureBounds(sprite, ignoreY) {
-        if (sprite.x < this.startX) {
-            sprite.x = this.startX;
-        }
-        if (!ignoreY && sprite.y < this.startY) {
-            sprite.y = this.startY;
-        }
-        if (sprite.x + sprite.w > this.endX) {
-            sprite.x = this.endX - sprite.w;
-        }
-        if (!ignoreY && sprite.y + sprite.h > this.endY) {
-            sprite.y = this.endY - sprite.h;
-        }
-    }
-
-    addEnemy() {
-        let interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV01;
-        // if (iterations > ITERATIONS_LV04) {
-        //     interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV04;
-        // } else if (iterations > ITERATIONS_LV03) {
-        //     interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV03;
-        // } else if (iterations > ITERATIONS_LV02) {
-        //     interval = FREQUENCY_OF_ENEMY_APPEARANCE_LV02;
-        // }
-
-        if (Util.getRandom(interval) === 0) {
-            let elementName = "enemy" + Util.getRandom(10000000);
-            let enemy = new Sprite(
-                elementName,
-                Util.getRandom(450),
-                INIT_ENEMY_POSITION_Y,
-                ENEMY_SIZE_X,
-                ENEMY_SIZE_Y
-            );
-
-            let element = document.createElement("div");
-            element.id = enemy.id;
-            element.className = "enemy";
-            document.getElementById("container").appendChild(element);
-
-            this.pushEnemy(enemy);
-        }
-    }
-
-    newLaser() {
-        if (this.createdLastLaserAt < 20) {
-            return;
-        }
-        this.createdLastLaserAt = 0;
-        let laserName = "laser" + Util.getRandom(10000000);
-        /**
-         * theory of calculation
-         *     laser.x = hero.x + (hero.w / 2) - (laser.w / 2)
-         *     laser.y = hero.y - laser.h
-         * But actually these thoery doesn't work on practice
-         * So use correction value
-         */
-        // "-3" is correction value
-        // "+25" is correction value
-        let laser = new Sprite(
-            laserName,
-            game.match.field.hero.x +
-                game.match.field.hero.w / 2 -
-                LASER_SIZE_X / 2 -
-                3,
-            game.match.field.hero.y - LASER_SIZE_Y + 25,
-            LASER_SIZE_X,
-            LASER_SIZE_Y
+class Pawn extends Enemy {
+    constructor(name) {
+        const INIT_ENEMY_POSITION_Y = -40;
+        const ENEMY_SIZE_X = 35;
+        const ENEMY_SIZE_Y = 35;
+        super(
+            name,
+            Util.getRandom(450),
+            INIT_ENEMY_POSITION_Y,
+            ENEMY_SIZE_X,
+            ENEMY_SIZE_Y
         );
-
-        let laserElm = document.createElement("div");
-        laserElm.id = laser.id;
-        laserElm.className = "laser";
-        document.getElementById("container").appendChild(laserElm);
-
-        game.match.field.pushLaser(laser);
     }
+}
 
-    showSprites() {
-        this.hero.setPosition();
-        for (let i = 0; i < this.lasers.length; i++) {
-            this.lasers[i].setPosition();
-        }
-        for (let i = 0; i < this.enemies.length; i++) {
-            this.enemies[i].setPosition();
-        }
+class Laser extends Sprite {
+    constructor(id, x, y, w, h) {
+        super(id, x, y, w, h);
     }
+}
 
-    checkCollisions() {
-        for (let i = 0; i < this.enemies.length; i++) {
-
-            for (let y = 0; y < this.lasers.length; y++) {
-                if (intersects(this.lasers[y], this.enemies[i])) {
-                    document.getElementById(this.enemies[i].id).remove();
-                    this.enemies.splice(i, 1);
-                    i--;
-                    game.match.score += BASIC_SCORE_POINT;
-                    // remove laser
-                    document.getElementById(this.lasers[y].id).remove();
-                    this.lasers.splice(y, 1);
-                    y--;
-                }
-            }
-        }
-
-        for (let i = 0; i < this.enemies.length; i++) {
-            if (intersects(this.hero, this.enemies[i])) {
-                game.gameOver();
-            } else if (
-                this.enemies[i].y + this.enemies[i].h >=
-                SCREEN_BOTTOM + 40
-            ) {
-                document.getElementById(this.enemies[i].id).remove();
-                this.enemies.splice(i, 1);
-                i--;
-            }
-        }
-
-        for (let y = 0; y < this.lasers.length; y++) {
-            if (this.lasers[y].y + this.lasers[y].h <= SCREEN_TOP) {
-                document.getElementById(this.lasers[y].id).remove();
-                this.lasers.splice(y, 1);
-                y--;
-            }
-        }
+class NormalLaser extends Laser {
+    constructor(id, heroX, heroY) {
+        const LASER_W = 2;
+        const LASER_H = 50;
+        // "-3" is correction value
+        const x = heroX - LASER_W / 2 - 3;
+        // "+25" is correction value
+        const y = heroY - LASER_H + 25;
+        super(id, x, y, LASER_W, LASER_H);
     }
 }
 
@@ -394,25 +397,33 @@ document.onkeyup = function(e) {
 };
 
 class View {
-    updatePosition() {
-        for (let i = 0; i < game.match.field.enemies.length; i++) {
-            game.match.field.enemies[i].y += 4;
-            game.match.field.enemies[i].x += Util.getRandom(7) - 3;
-            game.match.field.ensureBounds(game.match.field.enemies[i], true);
-        }
-
-        for (let i = 0; i < game.match.field.lasers.length; i++) {
-            game.match.field.lasers[i].y -= 12;
-            game.match.field.ensureBounds(game.match.field.lasers[i], true);
-        }
-    }
-
     static setVisible(elm) {
         elm.style.visibility = "visible";
     }
 
     static setHidden(elm) {
         elm.style.visibility = "hidden";
+    }
+
+    static setPosition(id, x, y) {
+        let elm = document.getElementById(id);
+        elm.style.left = x + "px";
+        elm.style.top = y + "px";
+    }
+
+    static setInnerHtml(elm, content) {
+        elm.innerHTML = content;
+    }
+
+    static createElementInContainer(id, className) {
+        let elm = document.createElement("div");
+        elm.id = id;
+        elm.className = className;
+        document.getElementById("container").appendChild(elm);
+    }
+
+    static remove(id) {
+        document.getElementById(id).remove();
     }
 }
 
@@ -425,21 +436,21 @@ class Util {
 function loop() {
     const time = new Date().getTime();
     if (time - game.match.lastLoopRun > 40) {
-        view.updatePosition();
+        game.match.updatePosition();
         game.handleControls();
-        game.match.field.checkCollisions();
+        game.match.checkCollisions();
 
         if (game.match.ended === false) {
-            game.match.field.addEnemy();
+            game.match.addEnemy();
         }
 
-        game.match.field.showSprites();
+        game.match.showSprites();
         game.match.showScore();
 
         game.match.lastLoopRun = time;
         game.match.iterations++;
 
-        game.match.field.createdLastLaserAt++;
+        game.match.createdLastLaserAt++;
     }
     setInterval("loop()", 20);
 }
